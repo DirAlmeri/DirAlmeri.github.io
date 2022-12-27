45e84db8cc5a798fa505e9132d2e698c1a14bbcb
Dir_Almery <92209281+DirAlmeri@users.noreply.github.com>
2022-12-27 14:25:40 +0300

    Add files via upload

---
+++ project/src/components/PageBody.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <BodyCompetencies />
+    <BodyTeam />
+    <BodyTariffs />
+    <BodyLastCase />
+  </div>
+</template>
+  
+<script>
+import BodyCompetencies from './bodyComponents/BodyCompetencies.vue'
+import BodyLastCase from './bodyComponents/BodyLastCase.vue'
+import BodyTeam from './bodyComponents/BodyTeam.vue'
+import BodyTariffs from './bodyComponents/BodyTariffs.vue'
+
+export default {
+  name: 'PageBody',
+  components: {
+    BodyCompetencies,
+    BodyLastCase,
+    BodyTeam,
+    BodyTariffs
+  }
+
+}
+</script>
+
+<style scoped>
+</style>
